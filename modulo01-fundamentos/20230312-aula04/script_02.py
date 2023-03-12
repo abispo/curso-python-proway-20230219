74d306812ec3dccd7d8317d46036994789e9a440
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-03-12 18:25:35 -0300

    Arquivos aula 04 Python Fundamentos

--- modulo01-fundamentos/20230312-aula04/script_02.py
+++ modulo01-fundamentos/20230312-aula04/script_02.py
@@ -23,3 +23,14 @@ if __name__ == "__main__":
 
         # O método writelines recebe uma lista de valores e insere no arquivo
         arquivo.writelines(["Python\n", "Golang\n", "Java\n", "PHP\n"])
+
+    # Quando abrimos o arquivo no modo a (append), se o arquivo não existir, ele é criado
+    # Caso o arquivo exista, o cursor é enviado para o final do arquivo, com isso sendo possível
+    # adicionar mais informações no arquivo
+    with open(os.path.join(os.getcwd(), "texto_02.txt"), "a", encoding="utf-8") as arquivo:
+
+        arquivo.write("Esse texto não sobrescreveu o arquivo")
+
+    # Também podemos abrir o arquivo ao mesmo tempo como leitura e escrita
+    with open(os.path.join(os.getcwd(), "texto_02.txt"), "r+") as arquivo:
+        print(arquivo.read())
