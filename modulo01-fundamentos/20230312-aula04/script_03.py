3fc4baf49ed2221dda35201e24c4807628533f91
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-03-12 18:57:09 -0300

    Arquivos aula 04 Python Fundamentos

---
+++ modulo01-fundamentos/20230312-aula04/script_03.py
@@ -0,0 +1,28 @@
+"""
+Trabalhando com arquivos .csv no Python
+
+reader e DictReader
+
+CSV -> Comma Separated Values -> Valores Separados por Vírgula
+"""
+
+import csv
+import os
+
+if __name__ == "__main__":
+    
+    with open(os.path.join(os.getcwd(), "notas.csv"), "r") as arquivo:
+
+        # Por padrão, o método reader considera que o separador padrão é a vírgula
+        # Nesse caso, precisamos indicar que o separador padrão é o ponto-e-vírgula
+        arquivo_csv = csv.reader(arquivo, delimiter=';')
+
+        for linha in arquivo_csv:
+            print(linha)
+
+    with open(os.path.join(os.getcwd(), "notas.csv"), "r") as arquivo:
+
+        arquivo_csv = csv.DictReader(arquivo, delimiter=';')
+
+        for linha in arquivo_csv:
+            print(linha)
---
+++ modulo01-fundamentos/20230312-aula04/script_04.py
@@ -0,0 +1,9 @@
+"""
+Trabalhando com arquivos .csv no Python
+
+writer e DictWriter
+
+https://docs.python.org/3/library/csv.html
+
+CSV -> Comma Separated Values -> Valores Separados por Vírgula
+"""
