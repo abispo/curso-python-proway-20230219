5b6cf49b76421841b17b3335029b440c44df84a5
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-04-02 13:53:10 -0300

    Arquivos aula 03 Python com Banco de Dados

--- modulo02-python-com-banco-de-dados/20230402-aula03/script03.sql
+++ modulo02-python-com-banco-de-dados/20230402-aula03/script03.sql
@@ -1,3 +1,8 @@
+/*
 A terceira forma normal (3FN) exige que cada coluna em uma tabela seja dependente apenas da chave primária da tabela, e não de
 outras colunas. Isso ajuda a evitar problemas de redundância e inconsistência de dados em relação a informações desnecessárias ou
 irrelevantes, e também ajuda a melhorar a eficiência de consultas ao banco de dados.
+
+*/
+
+-- tb_pedidos_itens
