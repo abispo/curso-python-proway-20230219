8ff671f9dea284e6f54ec0fbc77d1422029df434
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-04-09 17:20:50 -0300

    Arquivos aula 04 Python com Banco de Dados.

--- modulo02-python-com-banco-de-dados/20230409-aula04/script01.py
+++ modulo02-python-com-banco-de-dados/20230409-aula04/script01.py
@@ -45,3 +45,49 @@ if __name__ == "__main__":
     # Para inserir os dados na tabela, além de executar os commandos INSERT, precisamos "confirmar" a
     # transação utilizando o método commit() do objeto de conexão
     conexao.commit()
+
+    # Listando os dados que foram cadastrados
+
+    comando = "SELECT * FROM tb_estados"
+    resultado = cur.execute(comando)
+
+    # O método fetchone() trás apenas o primeiro resultado da consulta
+    # Sempre retorna uma tupla com os valores
+    print(resultado.fetchone())
+
+    # O método fetchmany(size) trás a quantidade de registros indicados como parâmetro size
+    print(resultado.fetchmany(10))
+
+    # O métdo fetchall() trás os registros restantes
+    print(resultado.fetchall())
+
+    # Os métodos fetchmany(size) e fetchall() retornam uma lista de tuplas
+
+    # É importante notar que quando utilizamos os métodos fetch, a posição do cursor muda de acordo
+    # com o método. Ou seja, se consumirmos todos os registros do objeto cursor, temos que executar
+    # o comando de consulta novamente.
+
+    # Trazendo novamente os dados
+    resultado = cur.execute(comando)
+
+    estados = resultado.fetchall()
+
+    print("Lista de Estados Brasileiros")
+
+    for estado in estados:
+        saida = f"""
+        Estado: {estado[1]}
+        Sigla: {estado[2]}"""
+
+        print(saida)
+
+    # Últimos comandos a serem executados
+    # Remove a tabela tb_estados
+    comando = "DROP TABLE tb_estados"
+    cur.execute(comando)
+    
+    # Fecha a conexão do cursor
+    cur.close()
+
+    # Fecha a conexão com o banco de dados
+    conexao.close()
