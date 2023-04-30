c3e9c2aef33f3595e271b7925951ed110615b17e
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-04-30 18:57:14 -0300

    Arquivos aula 01 python web

---
+++ modulo03-desenvolvimento-web-com-python/20230430-aula01/enquete/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EnqueteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'enquete'
---
+++ modulo03-desenvolvimento-web-com-python/20230430-aula01/enquete/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Pergunta(models.Model):
+
+    texto = models.CharField(max_length=200)
+    data_de_publicacao = models.DateTimeField("Data de publicação")
+
+
+    # A classe Meta representa as configurações da model
+    class Meta:
+        db_table = "tb_perguntas"
+
+
+class Opcao(models.Model):
+
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    texto = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = "tb_opcoes"
