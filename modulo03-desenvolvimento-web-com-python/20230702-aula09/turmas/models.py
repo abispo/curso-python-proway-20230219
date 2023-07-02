51db276b9849fdeb9cc12a3e47906f556053db6f
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-07-02 15:26:32 -0300

    Arquivos aula 09 Python web

--- modulo03-desenvolvimento-web-com-python/20230702-aula09/turmas/models.py
+++ modulo03-desenvolvimento-web-com-python/20230702-aula09/turmas/models.py
@@ -5,14 +5,15 @@ from cursos.models import Curso
 
 
 class Turma(models.Model):
-    
+
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
-    instrutor = models.ForeignKey(User, on_delete=models.CASCADE)
+    instrutor = models.ForeignKey(User, related_name="instrutor", on_delete=models.CASCADE)
     data_inicio = models.DateTimeField(null=False, blank=False)
     data_fim = models.DateTimeField(null=False, blank=False)
     qtd_minima_alunos = models.IntegerField(default=0)
     qtd_maxima_alunos = models.IntegerField(default=1000)
-
+    alunos = models.ManyToManyField(User, related_name="aluno", db_table="tb_turmas_alunos")
+    
 
     def __str__(self):
         return f"Turma {self.data_inicio.year}{self.id} {self.curso}"
