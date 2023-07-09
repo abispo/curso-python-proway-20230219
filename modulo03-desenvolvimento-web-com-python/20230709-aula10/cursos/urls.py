76d54520c18739ed4266fbcb16456702dcb9edf0
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-07-09 17:57:51 -0300

    Arquivos aula 10 python web.

--- modulo03-desenvolvimento-web-com-python/20230709-aula10/cursos/urls.py
+++ modulo03-desenvolvimento-web-com-python/20230709-aula10/cursos/urls.py
@@ -6,4 +6,5 @@ app_name = "cursos"
 
 urlpatterns = [
     path("", views.mostrar_todos_cursos, name="todos_os_cursos"),
+    path("<int:curso_id>/", views.detalhe_do_curso, name="detalhe_do_curso"),
 ]
--- modulo03-desenvolvimento-web-com-python/20230709-aula10/cursos/views.py
+++ modulo03-desenvolvimento-web-com-python/20230709-aula10/cursos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # import absoluto
 from cursos.models import Curso
@@ -13,3 +13,13 @@ def mostrar_todos_cursos(request):
     contexto = {"todos_os_cursos": todos_os_cursos}
 
     return render(request, "todos_os_cursos.html", contexto)
+
+
+def detalhe_do_curso(request, curso_id):
+    curso = get_object_or_404(Curso, pk=curso_id)
+
+    lista_de_turmas = curso.turma_set.filter(aceitando_matriculas=True)
+
+    contexto = {"lista_de_turmas": lista_de_turmas}
+
+    return render(request, "detalhe_curso.html", contexto)
